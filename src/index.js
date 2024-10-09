512293fe8f8e2fe3e88768180a0f85a162754205
bongiBong-create <mairanss543@inbox.ru>
2024-10-10 00:33:39 +0300

    layot + animation

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import "./style.css";
+
+console.log("hello")
+
+const theme = document.querySelector(".theme")
+
+theme.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+})
+
+
+function onEntry(entry) {
+  entry.forEach(change => {
+      if (change.isIntersecting) {
+          change.target.classList.add('element__show')
+      } else {
+          change.target.classList.remove('element__show')
+      }
+  });
+}
+
+let options = {
+  rootMargin: "0px",
+  threshold: .25,
+};
+
+let observer = new IntersectionObserver(onEntry, options);
+const elements = document.querySelectorAll('.animation__el');
+
+for (const el of elements) {
+  observer.observe(el);
+}
